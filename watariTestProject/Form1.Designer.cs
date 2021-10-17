eb6d97021782047188def32d9256af34a8b41001
watari <wataris0114@gmail.com>
2021-10-17 16:39:47 +0900

    iText使用

--- watariTestProject/Form1.Designer.cs
+++ watariTestProject/Form1.Designer.cs
@@ -29,17 +29,32 @@ namespace watariTestProject
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
+            ""}, -1, System.Drawing.Color.Empty, System.Drawing.SystemColors.InactiveCaptionText, null);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem("");
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem("");
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(287, 196);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(328, 261);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(82, 22);
+            this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "謎のボタン";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,35 +62,106 @@ namespace watariTestProject
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(277, 80);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(123, 36);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(110, 23);
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
             this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Meiryo UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(287, 220);
+            this.label1.Location = new System.Drawing.Point(328, 293);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(169, 35);
+            this.label1.Size = new System.Drawing.Size(213, 43);
             this.label1.TabIndex = 2;
             this.label1.Text = "押すな！！！";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(235, 369);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 104);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem5,
+            listViewItem6,
+            listViewItem7,
+            listViewItem8});
+            this.listView1.Location = new System.Drawing.Point(58, 352);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(151, 121);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(347, 36);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(540, 188);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 125;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(752, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(135, 62);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "iTextテスト";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(700, 338);
+            this.ClientSize = new System.Drawing.Size(952, 515);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +172,16 @@ namespace watariTestProject
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- watariTestProject/Form1.cs
+++ watariTestProject/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -34,12 +37,89 @@ namespace watariTestProject
             }
             textBox1.Text = Testbool.ToString();
 
-
+            dataGridView1.Rows.Add("ワタリ", Testbool.ToString());
 
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+
+//            String template = @"C:\WORK\test\template.pdf";
+//            String output = @"C:\WORK\test\output.pdf";
+
+            //A4サイズを横向きで
+            Document pdfDocument = new Document(PageSize.A4.Rotate(), 0, 0, 0, 0);
+
+            //出力先のファイル名
+            string makePdfFilePath = @"C:\WORK\test\output.pdf";
+            FileStream fileStream = new FileStream(makePdfFilePath, FileMode.Create);
+            PdfWriter pdfWriter = PdfWriter.GetInstance(pdfDocument, fileStream);
+
+
+            //PDFドキュメントを開く
+            pdfDocument.Open();
+
+            //フォント名
+            string fontName = "ＭＳ ゴシック";
+
+            //フォントサイズ
+            float fontSize = 10.0f;
+
+            //フォントスタイル。 複合するときは、&amp;で。
+            int fontStyle = iTextSharp.text.Font.BOLD;// &amp;iTextSharp.text.Font.ITALIC;
+
+            //フォントカラー
+            BaseColor baseColor = BaseColor.BLACK;
+
+            //Fontフォルダを指定
+            FontFactory.RegisterDirectory(Environment.SystemDirectory.Replace("system32", "fonts"));
+
+            //フォントの設定
+            iTextSharp.text.Font font =
+                FontFactory.GetFont(fontName,
+                BaseFont.IDENTITY_H,            //横書き
+                BaseFont.NOT_EMBEDDED,          //フォントを組み込まない
+                fontSize,
+                fontStyle,
+                baseColor);
+
+            PdfContentByte pdfContentByte = pdfWriter.DirectContent;
+
+            ColumnText columnText = new ColumnText(pdfContentByte);
+
+            //SetSimpleColumnで出力
+            columnText.SetSimpleColumn(
+                new Phrase("あいうえおかきくけこ", font)
+                , 50    // X1位置
+                , 50    // Y1位置
+                , 100   // X2位置
+                , 70    // Y2位置
+                , fontSize
+                , Element.ALIGN_LEFT    //ちなみに、SetSimpleColumnでは、ALIGN_MIDDLE（縦方向の中寄せ）は使えない
+                );
+
+            //テキスト描画
+            columnText.Go();
+
+            //PDFドキュメントを閉じる
+            pdfDocument.Close();
 
         }
     }
